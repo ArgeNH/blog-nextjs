9c34233b750e80c1763aa1d37706205aa570478d
Arge Niño <arge.nino@uptc.edu.co>
2020-08-13 00:06:21 -0500

    add aboutpage, cards and minimal details

--- pages/_app.js
+++ pages/_app.js
@@ -1,4 +1,6 @@
 import '../styles/globals.css'
+import '@material/tab/dist/mdc.tab.min.css'
+import '@material/tab-bar/dist/mdc.tab-bar.min.css'
 
 function MyApp({ Component, pageProps }) {
   return <Component {...pageProps} />
--- pages/index.js
+++ pages/index.js
@@ -1,18 +1,38 @@
 import Head from 'next/head'
+import Link from 'next/link'
 import styles from '../styles/Home.module.css'
+import { Tab, TabBar } from '@rmwc/tabs'
 
 export default function Home() {
   return (
     <div className={styles.container}>
       <Head>
-        <title> 🧑‍💻 English Blog - Arge Niño</title>
+        <title>English Blog - Arge Niño</title>
         <link rel="icon" href="/icon-arge.ico" />
       </Head>
 
       <main className={styles.main}>
+
+
+
         <h1 className={styles.title}>
-          Welcome to English blog of <a href="https://www.instagram.com/arge.nino/">Arge Niño</a>🧑‍💻
+          Welcome to English blog of
+          <br /><a href="https://www.instagram.com/arge.nino/">Arge Niño</a>🧑‍💻
         </h1>
+        
+        <div className={styles.grid}>
+         <a href="/activities" className={styles.card}>
+            <h3>Activities &rarr;</h3>
+            <p>Activities scheduled to upload to the blog.</p>
+          </a>
+
+          <a href="/about" className={styles.card}>
+            <h3>About Me &rarr;</h3>
+            <p>A brief description and various things. Small projects</p>
+          </a>
+
+        </div>
+
       </main>
 
       <footer className={styles.footer}>
