1415f3f99d1f280fb8c55ea0483f9972afadeb57
Arge Niño <arge.nino@uptc.edu.co>
2020-08-12 21:41:05 -0500

    second commit

--- pages/index.js
+++ pages/index.js
@@ -5,13 +5,13 @@ export default function Home() {
   return (
     <div className={styles.container}>
       <Head>
-        <title>Create Next App</title>
+        <title>English Blog - Arge Niño</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
       <main className={styles.main}>
         <h1 className={styles.title}>
-          Welcome to <a href="https://nextjs.org">Next.js!</a>
+          English V Blog - Arge Niño
         </h1>
 
         <p className={styles.description}>
