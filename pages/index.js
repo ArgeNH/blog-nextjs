45d2354fff26ace1f891892cd01d418cf8522377
Arge Niño <arge.nino@uptc.edu.co>
2020-08-12 21:58:16 -0500

    change index.js, add icon & svg

--- pages/index.js
+++ pages/index.js
@@ -6,58 +6,22 @@ export default function Home() {
     <div className={styles.container}>
       <Head>
         <title>English Blog - Arge Niño</title>
-        <link rel="icon" href="/favicon.ico" />
+        <link rel="icon" href="/icon-arge.ico" />
       </Head>
 
       <main className={styles.main}>
         <h1 className={styles.title}>
-          English V Blog - Arge Niño
+          Welcome to English blog of <a href="https://www.instagram.com/arge.nino/">Arge Niño</a>
         </h1>
-
-        <p className={styles.description}>
-          Get started by editing{' '}
-          <code className={styles.code}>pages/index.js</code>
-        </p>
-
-        <div className={styles.grid}>
-          <a href="https://nextjs.org/docs" className={styles.card}>
-            <h3>Documentation &rarr;</h3>
-            <p>Find in-depth information about Next.js features and API.</p>
-          </a>
-
-          <a href="https://nextjs.org/learn" className={styles.card}>
-            <h3>Learn &rarr;</h3>
-            <p>Learn about Next.js in an interactive course with quizzes!</p>
-          </a>
-
-          <a
-            href="https://github.com/vercel/next.js/tree/master/examples"
-            className={styles.card}
-          >
-            <h3>Examples &rarr;</h3>
-            <p>Discover and deploy boilerplate example Next.js projects.</p>
-          </a>
-
-          <a
-            href="https://vercel.com/import?filter=next.js&utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className={styles.card}
-          >
-            <h3>Deploy &rarr;</h3>
-            <p>
-              Instantly deploy your Next.js site to a public URL with Vercel.
-            </p>
-          </a>
-        </div>
       </main>
 
       <footer className={styles.footer}>
         <a
-          href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
+          href="https://github.com/ArgeNH"
           target="_blank"
-          rel="noopener noreferrer"
-        >
-          Powered by{' '}
-          <img src="/vercel.svg" alt="Vercel Logo" className={styles.logo} />
+          rel="noopener noreferrer">
+          Created by{' '}
+          <img src="/arge.svg" alt="Vercel Logo" className={styles.logo} />
         </a>
       </footer>
     </div>
