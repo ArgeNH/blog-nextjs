689d3840bd192a5a498b79f665a8f9b44e67b656
Arge Niño <arge.nino@uptc.edu.co>
2020-08-14 14:17:05 -0500

    changes in activity index

--- pages/activities/index.js
+++ pages/activities/index.js
@@ -1,28 +1,39 @@
 import styles from '../../styles/About.module.css'
 
-export default function Act(){
+export default function Act() {
     return (
         <>
-        
-        <main className={styles.main}>
+
+            <main className={styles.main}>
                 <h1>ACTIVITIES 📚</h1>
                 <div className='container'>
                     {/*Actividad 1*/}
                     <div class="card">
-                        <div class="card-body">
-                            <h3>Activity one</h3>
+                        <div class="card-body ">
+                            <h3>Activity One</h3>
                             <div class="row">
-                                <div class="col">
-                                    <iframe width="640" height="360" src="https://www.youtube.com/embed/CIpwuvEBiZI" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
-                                 </div>
-                                <div class="col">
-                                    <h6>Prueba</h6>
+                                <div class="col-sm ">
+                                    <div class={styles.video_responsive}>
+                                        <iframe src="https://www.youtube.com/embed/CIpwuvEBiZI" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+                                    </div>
+                                </div>
+                                <div class="col-sm">
+
+                                    <div class="card">
+                                        <div class="card-header">
+                                            Name of activity
+                                        </div>
+                                        <div class="card-body">
+                                            <h5 class="card-title">Title</h5>
+                                            <p class="card-text">Text...</p>
+                                        </div>
+                                    </div>
+
                                 </div>
                             </div>
                         </div>
                     </div>
                 </div>
-
             </main>
 
         </>
