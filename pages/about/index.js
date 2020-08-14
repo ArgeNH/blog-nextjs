d77e2f743a47b9de34a2a7d08126c2076eb4f769
Arge Niño <arge.nino@uptc.edu.co>
2020-08-14 12:58:26 -0500

    change

--- pages/about/index.js
+++ pages/about/index.js
@@ -5,24 +5,7 @@ export default function Act() {
     return (
 
         <>
-            <main className={styles.main}>
-                <h1>ACTIVITIES 📚</h1>
-                <div className='container'>
-                    <div class="card">
-                        <div class="card-body">
-                            <h3>Activity one</h3>
-                            <div class="row">
-                                <div class="col">
-                                    <iframe width="640" height="360" src="https://www.youtube.com/embed/CIpwuvEBiZI" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
-                                 </div>
-                                <div class="col">
-                                    <h6>Prueba</h6>
-                                </div>
-                            </div>
-                        </div>
-                    </div>
-                </div>
-            </main>
+            <h1>Hola</h1>
         </>
     );
 }
--- pages/activities/index.js
+++ pages/activities/index.js
@@ -1,8 +1,30 @@
+import styles from '../../styles/About.module.css'
+
 export default function Act(){
     return (
         <>
-        <h3>Activities</h3>
-        <iframe src="https://discordapp.com/widget?id=429682247231078400&theme=dark" width="350" height="500" allowtransparency="true" frameborder="0" sandbox="allow-popups allow-popups-to-escape-sandbox allow-same-origin allow-scripts"></iframe>
+        
+        <main className={styles.main}>
+                <h1>ACTIVITIES 📚</h1>
+                <div className='container'>
+                    {/*Actividad 1*/}
+                    <div class="card">
+                        <div class="card-body">
+                            <h3>Activity one</h3>
+                            <div class="row">
+                                <div class="col">
+                                    <iframe width="640" height="360" src="https://www.youtube.com/embed/CIpwuvEBiZI" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+                                 </div>
+                                <div class="col">
+                                    <h6>Prueba</h6>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+
+            </main>
+
         </>
     );
 }
